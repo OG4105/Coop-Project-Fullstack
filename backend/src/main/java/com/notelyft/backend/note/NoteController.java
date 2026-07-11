62d06c7b38068405615539e6bf528a1cbe80815b
Omar Gradascevic <og3150@rit.edu>
2026-07-11 23:54:17 +0200

    Complete notes CRUD (GET-one, PUT, DELETE)

--- backend/src/main/java/com/notelyft/backend/note/NoteController.java
+++ backend/src/main/java/com/notelyft/backend/note/NoteController.java
@@ -2,8 +2,11 @@ package com.notelyft.backend.note;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,10 +28,36 @@ public class NoteController {
         return noteRepository.findAll();
     }
 
+    // GET one method
+    @GetMapping("/{id}") //Finds by id
+    public Note noteGetOne( @PathVariable Long id){
+        return noteRepository.findById(id).orElseThrow(); // finds by ID, else throws an error
+    }
+
     //Post create method
     @PostMapping
     public Note postNote( @RequestBody Note note){
         return noteRepository.save(note);
+    }
+
+    // Post DELETE method
+    @DeleteMapping("/{id}")
+    public void deleteNote(@PathVariable Long id){
+        noteRepository.deleteById(id);
+    }
+
+    // Post PUT method
+    @PutMapping("/{id}")
+    public Note putNote(@PathVariable Long id, @RequestBody Note updatedNote) { // @updatedNote, New title/content user submits
+
+        Note note = noteRepository.findById(id).orElseThrow(); // loads the real note, otherwise throw
+
+        // change only the editable fields (Title and Content, Dates are NOT changable)
+        note.setTitle(updatedNote.getTitle());
+        note.setContent(updatedNote.getContent());
+        // UpdateTimeStamp updates automatically, so it does get changed, just not here
+
+        return noteRepository.save(note); // save the changes
     }
 
 }
