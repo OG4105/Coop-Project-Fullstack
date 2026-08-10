7693cc723ade72b7e1cf92295a343b2a576d3bbb
Omar Gradascevic <og3150@rit.edu>
2026-08-10 23:45:30 +0200

    Add create and delete notes in UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,50 @@ function App() {
 
   const [notes, setNotes] = useState([]);
 
-  // Function that runs once
-  useEffect(() => {
-    fetch("http://localhost:8080/api/notes") // Fetches GET Data from Backend
-      .then(res => res.json()) //Parses the JSON response body from Backend
-      .then(data => setNotes(data)); // Stores notes in state
-  }, []);
+  // Function that reloads fetch from Backend
+  const fetchNotes = () => {
+  fetch("http://localhost:8080/api/notes")
+    .then(res => res.json())
+    .then(data => setNotes(data));
+  };
+
+  useEffect(() => { fetchNotes(); }, []); // Use effect for fetching notes
+
+  const [title, setTitle] = useState(""); // Use state (Title)
+  const [content, setContent] = useState(""); // Use state (Content)
+
+  // Function ADD handler
+  const handleAdd = () => {
+    fetch("http://localhost:8080/api/notes", { //Fetches from site notes
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, content })
+    })
+      .then(() => { fetchNotes(); setTitle(""); setContent(""); }); //Fetches title and content
+  };
+
+  // Function DELETE handler
+  const handleDelete = (id) => { // Search based on ID
+    fetch(`http://localhost:8080/api/notes/${id}`, { method: "DELETE" })
+      .then(() => fetchNotes());
+  };
 
 
   return (
   <div>
+
     <h1>NoteLyft</h1>
+
+      <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Title" />
+      <input value={content} onChange={e => setContent(e.target.value)} placeholder="Content" />
+      <button onClick={handleAdd}>Add note</button>
+
     <ul>
       {notes.map(note => ( // map each note to an <li>; key={note.id} gives React a unique id per item
         <li key={note.id}>
-          <h2><strong>{note.title}</strong></h2> <p>{note.content}</p>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
+          <button onClick={() => handleDelete(note.id)}>Delete</button>
         </li>
       ))}
     </ul>
